93ea07c4eefc4e770ef71da2e92c600c1041f806
DivyanshiSinghal <divyanshisinghal95@gmail.com>
2019-04-24 11:55:43 +0530

    Update AreaInterest.java

--- src/main/java/com/example/demo/model/AreaInterest.java
+++ src/main/java/com/example/demo/model/AreaInterest.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
-@Table(name="areainterest")
+@Table(name="areainterests")
 public class AreaInterest {
 	@Id
 	@Column(name="areaid")
